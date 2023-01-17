0d299b051e94b8d44c198276327b49b0fedd2f38
Dalalghesh <Dalal.ghesh@gmail.com>
2023-01-17 03:15:33 +0300

    Revert "fay+ dalal"
    This reverts commit 51fc93581f9cb75715d773cc9190accf6ed80858.

--- belly_rate/lib/auth/our_user_model.dart
+++ belly_rate/lib/auth/our_user_model.dart
@@ -2,6 +2,5 @@ class OurUser{
   String first_name ;
   String last_name ;
   String phone_number ;
-  String picture;
-  OurUser({required this.last_name ,required this.picture, required this.first_name , required this.phone_number});
+  OurUser({required this.last_name , required this.first_name , required this.phone_number});
 }
--- belly_rate/lib/myProfile.dart
+++ belly_rate/lib/myProfile.dart
@@ -1,17 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:belly_rate/auth/our_user_model.dart';
-import 'package:belly_rate/auth/signin_page.dart';
-import 'package:belly_rate/auth/signup_page.dart';
-import 'package:belly_rate/updateProfile.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:belly_rate/auth/signin_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class myProfile extends StatefulWidget {
   myProfile({Key? key}) : super(key: key);
@@ -20,40 +8,8 @@ class myProfile extends StatefulWidget {
 }
 
 class _myProfile extends State<myProfile> {
-  User? user;
-  OurUser? ourUser;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    user = FirebaseAuth.instance.currentUser!;
-    print("currentUser: ${user?.uid}");
-
-    Future.delayed(Duration.zero).then((value) async {
-      var vari = await FirebaseFirestore.instance
-          .collection("Users")
-          .doc(user!.uid)
-          .get();
-      // Map<String,dynamic> userData = vari as Map<String,dynamic>;
-      print("currentUser: ${vari.data()}");
-
-      ourUser = OurUser(
-        first_name: vari.data()!['first_name'],
-        last_name: vari.data()!['last_name'],
-        picture: vari.data()!['picture'],
-        phone_number: vari.data()!['phone_number'],
-      );
-      setState(() {});
-    });
-
-    // User? user =  FirebaseAuth.instance.currentUser;
-  }
-
   @override
   Widget build(BuildContext context) {
-    final Color txt_color = Color(0xFF5a3769);
-    final Color button_color = Color.fromARGB(255, 216, 107, 147);
-    final double heightM = MediaQuery.of(context).size.height / 30;
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
@@ -87,12 +43,7 @@ class _myProfile extends State<myProfile> {
                       content: Text("Are you sure you want to logout?"),
                       actions: <Widget>[
                         TextButton(
-                          onPressed: () async {
-                            await FirebaseAuth.instance.signOut();
-                            Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(
-                                  builder: (context) => const SignIn()),
-                            );
+                          onPressed: () {
                             //action code for "Yes" button
                             // await FirebaseAuth.instance.signOut();
                             // Navigator.of(context).pushReplacement(
@@ -131,101 +82,6 @@ class _myProfile extends State<myProfile> {
           )
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Spacer(),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(100.0),
-              child: Image.network(
-                "${ourUser?.picture}",
-                fit: BoxFit.cover,
-                height: 150.0,
-                width: 150.0,
-                // errorBuilder: ,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
-              child: Text(
-                "${ourUser?.first_name}",
-                style: getMyTextStyle(
-                    txt_color: txt_color, fontSize: heightM * 0.75),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-              child: Text(
-                "${ourUser?.phone_number}",
-                style: getMyTextStyle(
-                    txt_color: Colors.grey, fontSize: heightM * 0.6),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 12),
-              child: Center(
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  height: heightM * 1.5,
-                  child: Material(
-                    elevation: 10.0,
-                    borderRadius: BorderRadius.circular(5.0), //12
-                    color: Colors.transparent, //Colors.cyan.withOpacity(0.5),
-                    child: MaterialButton(
-                      minWidth: MediaQuery.of(context).size.width,
-                      color: button_color,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.0)),
-                      splashColor: button_color,
-                      onPressed: () async {
-                        /// UpdateProfile
-                        Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        UpdateProfile(ourUser: ourUser)))
-                            .then((value) async {
-                          if (value) {
-                            var vari = await FirebaseFirestore.instance
-                                .collection("Users")
-                                .doc(user!.uid)
-                                .get();
-                            // Map<String,dynamic> userData = vari as Map<String,dynamic>;
-                            print("currentUser: ${vari.data()}");
-
-                            ourUser = OurUser(
-                              last_name: vari.data()!['last_name'],
-                              first_name: vari.data()!['first_name'],
-                              picture: vari.data()!['picture'],
-                              phone_number: vari.data()!['phone_number'],
-                            );
-                            setState(() {});
-                          }
-                        });
-                      },
-                      child: Text('Update Profile',
-                          textAlign: TextAlign.center,
-                          style: getMyTextStyle(
-                              txt_color: Colors.white,
-                              fontSize: heightM * 0.6)),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Spacer(),
-            Spacer(),
-            Spacer(),
-          ],
-        ),
-      ),
     );
-  }
-
-  TextStyle getMyTextStyle({required Color txt_color, double fontSize = 22}) {
-    return GoogleFonts.cairo(
-        color: txt_color, fontSize: fontSize, fontWeight: FontWeight.bold);
   }
 }
