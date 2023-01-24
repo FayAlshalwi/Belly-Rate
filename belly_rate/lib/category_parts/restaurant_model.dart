8640b72ddefd2ceb800e0f77e47ce43eb66a7c24
RaghadSD <raghadalrajhi@hotmail.com>
2023-01-24 18:49:36 +0300

    Update restaurant_model.dart

--- belly_rate/lib/category_parts/restaurant_model.dart
+++ belly_rate/lib/category_parts/restaurant_model.dart
@@ -4,7 +4,10 @@
 
 import 'dart:convert';
 
-Restaurant restaurantFromJson(String str) => Restaurant.fromJson(json.decode(str));
+import '../models/rateModel.dart';
+
+Restaurant restaurantFromJson(String str) =>
+    Restaurant.fromJson(json.decode(str));
 
 String restaurantToJson(Restaurant data) => json.encode(data.toJson());
 
@@ -21,6 +24,7 @@ class Restaurant {
     this.category,
     this.photos,
     this.far,
+    this.rate,
   });
 
   String? priceAvg;
@@ -30,36 +34,42 @@ class Restaurant {
   String? location;
   String? id;
   double? far;
-  String? lat ;
-  String? long ;
+  String? lat;
+  String? long;
+  Rate? rate;
   String? category;
   List<String>? photos;
 
   factory Restaurant.fromJson(Map<String, dynamic> json) => Restaurant(
-    priceAvg: json["priceAvg"] == null ? null : json["priceAvg"],
-    phoneNumber: json["phoneNumber"] == null ? null : json["phoneNumber"],
-    name: json["name"] == null ? null : json["name"],
-    description: json["description"] == null ? null : json["description"],
-    location: json["location"] == null ? null : json["location"],
-    id: json["ID"] == null ? null : json["ID"],
-    far: json["far"],
-    lat: json["lat"] == null ? null : json["lat"],
-    long: json["long"] == null ? null : json["long"],
-    category: json["category"] == null ? null : json["category"],
-    photos: json["photos"] == null ? null : List<String>.from(json["photos"].map((x) => x)),
-  );
+        priceAvg: json["priceAvg"] == null ? null : json["priceAvg"],
+        phoneNumber: json["phoneNumber"] == null ? null : json["phoneNumber"],
+        name: json["name"] == null ? null : json["name"],
+        description: json["description"] == null ? null : json["description"],
+        location: json["location"] == null ? null : json["location"],
+        id: json["ID"] == null ? null : json["ID"],
+        far: json["far"],
+        lat: json["lat"] == null ? null : json["lat"],
+        long: json["long"] == null ? null : json["long"],
+        rate: json["rate"],
+        category: json["category"] == null ? null : json["category"],
+        photos: json["photos"] == null
+            ? null
+            : List<String>.from(json["photos"].map((x) => x)),
+      );
 
   Map<String, dynamic> toJson() => {
-    "priceAvg": priceAvg == null ? null : priceAvg,
-    "phoneNumber": phoneNumber == null ? null : phoneNumber,
-    "name": name == null ? null : name,
-    "description": description == null ? null : description,
-    "location": location == null ? null : location,
-    "ID": id == null ? null : id,
-    "lat": lat == null ? null : lat,
-    "long": long == null ? null : long,
-    "category": category == null ? null : category,
-    "far": far,
-    "photos": photos == null ? null : List<dynamic>.from(photos!.map((x) => x)),
-  };
+        "priceAvg": priceAvg == null ? null : priceAvg,
+        "phoneNumber": phoneNumber == null ? null : phoneNumber,
+        "name": name == null ? null : name,
+        "description": description == null ? null : description,
+        "location": location == null ? null : location,
+        "ID": id == null ? null : id,
+        "rate": rate,
+        "lat": lat == null ? null : lat,
+        "long": long == null ? null : long,
+        "category": category == null ? null : category,
+        "far": far,
+        "photos":
+            photos == null ? null : List<dynamic>.from(photos!.map((x) => x)),
+      };
 }
