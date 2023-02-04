c38e8ee717ab09bf19b4644041e06262aae4b441
Dalalghesh <Dalal.ghesh@gmail.com>
2023-02-05 01:28:05 +0300

    Performance

--- belly_rate/test/model/Update_profile_Test.dart
+++ belly_rate/test/model/Update_profile_Test.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:belly_rate/main.dart';
-import 'package:mockito/mockito.dart';
 import 'package:rxdart/rxdart.dart';
 
 void main() {
